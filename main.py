7a33426e656590874a69c847162f559b6fa8b03a
acm <a-mango@users.noreply.github.com>
2024-01-29 20:08:28 +0100

    rfr: credentials in system keyring

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import math
 import requests
 import pandas as pd
@@ -28,22 +29,71 @@ df = pd.DataFrame(
 )
 
 
-# Save the credentials to system keyring
 def save_credentials():
-    # TODO check that no credentials exist, otherwise override
-    print("Your AAI credentials will be stored in the system keyring.")
-    username = input("Input your username (first.last): ")
+    """
+    Save the user's credentials to the system keyring. Exits upon failure
+    """
+
+    logging.info("Your AAI credentials will be stored in the system keyring.")
+    username = input("Enter your username (first.last): ")
     password = getpass.getpass("Enter your password: ")
-    kr.set_password(APP_NAME, username, password)  # TODO add try/except
+
+    # Validate the input
+    if not username or not password:
+        logging.error("You must enter a username and a password")
+        exit(1)
+    elif not re.match(r"^[a-z]+\.[a-z]+$", username):
+        logging.error("The username must be in the format first.last")
+        exit(1)
+
+    # Save the credentials to the system keyring
+    try:
+        kr.set_password(APP_NAME, username, password)
+    except kr.errors.KeyringError as e:
+        logging.error("Failed to store credentials in keychain: %s", e)
+        exit(1)
+    except kr.errors.PasswordSetError:
+        logging.error("An error occured while saving the credentials")
+        exit(1)
+    except Exception as e:
+        logging.error("An unknown error occured while saving the credentials: %s", e)
+        exit(1)
+    else:
+        logging.info("Credentials successfully saved to system keyring")
 
 
-# Could use some error handling
 def get_credentials():
-    cred = kr.get_credential(APP_NAME, None)
+    """
+    Retrieve the user's credentials from the system keyring. Exits upon failure.
+
+    :return: A keyring.Credential object containing the username and password
+    :rtype: keyring.Credential
+    """
+    cred = None
+
+    # Try to retrieve the credentials from the system keyring
+    try:
+        cred = kr.get_credential(APP_NAME, None)
+    except kr.errors.KeyringError as e:
+        logging.error("Failed to retrieve credentials from keychain: %s", e)
+    except Exception as e:
+        logging.error("An unexpected error occurred: %s", e)
+
+    # If the credentials were not found, ask the user to save them
     if cred is None:
         save_credentials()
-        cred = kr.get_credential(APP_NAME, "")
-    assert cred is not None
+        try:
+            cred = kr.get_credential(APP_NAME, "")
+        except kr.errors.KeyringError as e:
+            logging.error("Failed to retrieve credentials from keychain after saving: %s", e)
+        except Exception as e:
+            logging.error("An unexpected error occurred: %s", e)
+
+    # If the credentials are still not found, exit
+    if cred is None:
+        logging.error("Failed to retrieve credentials. Please check your keychain.")
+        exit(1)
+
     return cred
 
 
