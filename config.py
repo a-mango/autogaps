dd7fa4ef84d4e1708a9602dbb727cea438d8c7a7
acm <a-mango@users.noreply.github.com>
2024-01-30 11:24:28 +0100

    rfr: request headers

--- config.py
+++ config.py
@@ -2,8 +2,9 @@ APP_NAME = "autogaps"
 URL_BASE = "https://gaps.heig-vd.ch/consultation.php"
 URL = "https://gaps.heig-vd.ch/consultation/controlescontinus/consultation.php"
 HEADER_DATA = {
-    # "Accept-Encoding": "gzip, deflate, br"
-    # "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0"
-    # "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    # "Accept-Language": "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3",
+    "Accept-Encoding": "gzip, deflate, br",
+    # This should give someone a heart attack >:)
+    "User-Agent": "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0)",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3",
 }
